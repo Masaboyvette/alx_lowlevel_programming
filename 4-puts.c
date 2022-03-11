ba19b32beb3084d66462af96a9068c69607999d5
Yvette <99931201+Masaboyvette@users.noreply.github.com>
2022-03-11 04:54:06 +0200

    Add files via upload

--- 4-puts.c
+++ 4-puts.c
@@ -1,1 +1,10 @@
-
+ #include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+   char string[90];
+  strcpy(string, "The Programming is like building a multilingual puzzle");
+   puts(string);
+   return 0;
+}
